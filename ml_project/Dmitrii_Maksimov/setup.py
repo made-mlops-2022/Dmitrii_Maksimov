849326c92da19f7b14239512d014fd98e8e91cab
Maksimov-Dmitry <61335724+Maksimov-Dmitry@users.noreply.github.com>
2022-10-30 09:46:36 +0100

    structure of project is created

---
+++ ml_project/Dmitrii_Maksimov/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='First homework on MLOPS at the MADE',
+    author='Dmitrii Maksimov',
+    license='',
+)
